894ac8d6c27dfabf28ddf2ce45b6bb84af53df09
woshiqingteng <1509320578@qq.com>
2026-09-20 23:06:41 +0800

    refactor: root add_subdirectory layers, global flags in toolchain, cache-internal module vars, apps preset, openocd from path

--- cmake/toolchain/arm_gcc.cmake
+++ cmake/toolchain/arm_gcc.cmake
@@ -1,4 +1,5 @@
-# arm-none-eabi-gcc toolchain.
+# arm-none-eabi-gcc toolchain. Global compile/link flags live in cpu/ and are
+# pulled in here (before project() enables the language).
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
@@ -10,3 +11,14 @@ set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# BOARD/CPU come from the preset cache; provide defaults for nested try_compile.
+if(NOT BOARD)
+    set(BOARD "openedv_stm32f4")
+endif()
+include("${CMAKE_CURRENT_LIST_DIR}/../board/${BOARD}.cmake")
+
+if(NOT CPU)
+    set(CPU "cortex-m4")
+endif()
+include("${CMAKE_CURRENT_LIST_DIR}/../cpu/${CPU}.cmake")
