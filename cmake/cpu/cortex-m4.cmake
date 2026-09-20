785635dacb076cf09d8ac820ee871f82618d0d12
woshiqingteng <1509320578@qq.com>
2026-09-20 23:08:28 +0800

    refactor: select board/cpu/toolchain in cmake list; move objcopy/objdump to toolchain

--- cmake/cpu/cortex-m4.cmake
+++ cmake/cpu/cortex-m4.cmake
@@ -1,11 +1,14 @@
-# Cortex-M4F global compile/link flags (applied through the toolchain).
+# Cortex-M4F flags. Applied globally via the CMAKE_* cache (this file is
+# included from cmake/CMakeLists.txt, before any target is created).
 
-set(CMAKE_C_FLAGS_INIT
-    "-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard \
--std=gnu11 -ffunction-sections -fdata-sections -Wall -Wextra")
-set(CMAKE_C_FLAGS_DEBUG_INIT "-Og -g3 -DDEBUG")
-set(CMAKE_C_FLAGS_RELEASE_INIT "-O2 -DNDEBUG")
-set(CMAKE_EXE_LINKER_FLAGS_INIT
-    "-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard \
--Wl,--gc-sections -Wl,--no-warn-rwx-segments -specs=nano.specs -specs=nosys.specs \
--Wl,--print-memory-usage")
+set(_cpu_flags "-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard")
+
+set(CMAKE_C_FLAGS
+    "${_cpu_flags} -std=gnu11 -ffunction-sections -fdata-sections -Wall -Wextra"
+    CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_DEBUG "-Og -g3 -DDEBUG" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_RELEASE "-O2 -DNDEBUG" CACHE STRING "" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS
+    "${_cpu_flags} -Wl,--gc-sections -Wl,--no-warn-rwx-segments \
+-specs=nano.specs -specs=nosys.specs -Wl,--print-memory-usage"
+    CACHE STRING "" FORCE)
